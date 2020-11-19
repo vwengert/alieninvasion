77efe2142750d9a9ca3fbcc7af6fe1de19074f2e
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2020-11-19 07:13:35 +0100

    Game class and initial Window

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game ressources."""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
