60b1680eaa1ecf98f99aebd7c8c84184eab742bf
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2020-11-20 16:07:18 +0100

    added bullets and handling of them

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -11,23 +12,20 @@ class AlienInvasion:
         """Initialize the game, and create game ressources."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
-
-    def _update_screen(self):
-        """Respond to keypress and mouse events."""
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-        pygame.display.flip()
 
     def _check_events(self):
         """Update images on the scene, and flip to the new scene."""
@@ -35,17 +33,49 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_event(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_event(event)
 
+    def _check_keydown_event(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_event(self, event):
+        """Respond to key release."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _fire_bullet(self):
+        """Fire a bullet as long you are below the limit."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update your bullets and remove them after leaving the screen."""
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        """Respond to keypress and mouse events."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -6,5 +6,11 @@ class Settings():
         # Bildschirmeinstellungen
         self.screen_width = 1200
         self.screen_height = 800
-        self.ship_speed = 0.5
+        self.ship_speed = 1.5
         self.bg_color = (230, 230, 230)
+        # Geschosseinstellungen
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 5
